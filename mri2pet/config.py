da2bfeee58864aaae1e22972d6cc7858592a2c68
PeiwangL <113399216+Loewy25@users.noreply.github.com>
2025-09-01 22:20:27 -0500

    Update config.py

--- mri2pet/config.py
+++ mri2pet/config.py
@@ -6,7 +6,7 @@ import torch
 ROOT_DIR   = "/scratch/l.peiwang/kari_brainv11"
 OUT_DIR    = "/home/l.peiwang/MRI2PET"
 
-RUN_NAME   = "test2_ssim"
+RUN_NAME   = "test_better strcture"
 OUT_RUN    = os.path.join(OUT_DIR, RUN_NAME)
 CKPT_DIR   = os.path.join(OUT_RUN, "checkpoints")
 VOL_DIR    = os.path.join(OUT_RUN, "volumes")
