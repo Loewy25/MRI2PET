2d940d8e70fe6b5205019a80b95710bfa0c29044
PeiwangL <113399216+Loewy25@users.noreply.github.com>
2025-12-02 23:11:38 -0600

    Update RUN_NAME to tau_MGDA_UB_v33_500_1_5

--- mri2pet/config.py
+++ mri2pet/config.py
@@ -6,7 +6,7 @@ import torch
 ROOT_DIR   = "/scratch/l.peiwang/braak_merged_300"
 OUT_DIR    = "/home/l.peiwang/MRI2PET"
 
-RUN_NAME   = "tau_MGDA_UB_v33_500_1_4"
+RUN_NAME   = "tau_MGDA_UB_v33_500_1_5"
 OUT_RUN    = os.path.join(OUT_DIR, RUN_NAME)
 CKPT_DIR   = os.path.join(OUT_RUN, "checkpoints")
 VOL_DIR    = os.path.join(OUT_RUN, "volumes")
