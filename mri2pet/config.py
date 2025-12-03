373f494d7531ab142d35c82af08953ef019cb0dd
PeiwangL <113399216+Loewy25@users.noreply.github.com>
2025-12-02 23:43:08 -0600

    Update config.py

--- mri2pet/config.py
+++ mri2pet/config.py
@@ -6,7 +6,7 @@ import torch
 ROOT_DIR   = "/scratch/l.peiwang/kari_brainv33_top300"
 OUT_DIR    = "/home/l.peiwang/MRI2PET"
 
-RUN_NAME   = "lse_mgda_ub_5"
+RUN_NAME   = "mgda_ub_5"
 OUT_RUN    = os.path.join(OUT_DIR, RUN_NAME)
 CKPT_DIR   = os.path.join(OUT_RUN, "checkpoints")
 VOL_DIR    = os.path.join(OUT_RUN, "volumes")
